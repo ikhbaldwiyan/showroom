562b2ee0cb821bb1cfaab5ab74ebdacf80bf4efe
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2021-03-03 16:03:43 +0700

    update elements button

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -4,11 +4,12 @@ import propTypes from 'prop-types';
 
 export default function Button(props) {
   const className = [props.className]
-  if (props.isPrimary) className.push('bnt-primary')
-  if (props.isLarge) className.push('bnt-lg')
-  if (props.isSmall) className.push('bnt-sm')
-  if (props.isBlock) className.push('bnt-block')
-  if (props.hasShadow) className.push('bnt-shadow')
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
 
   const onClick = () => {
     if (props.onClick) props.onClick()
@@ -18,7 +19,14 @@ export default function Button(props) {
     if (props.isDisabled) className.push("disabled")
     return (
       <span className={className.join(" ")} style={props.style}>
-        {props.isLoading ? <span className="spinner-border-sm mx-5"></span> : props.children}
+        {props.isLoading ? (
+          <>
+            <span className="spinner-border spinner-border-sm mx-5"></span>
+            <span className="sr-only">Loading...</span>
+          </>
+        ) : (
+          props.children
+        )}
       </span>
     )
   }
@@ -26,46 +34,54 @@ export default function Button(props) {
   if (props.type == "link") {
     if (props.isExternal) {
       return (
-        <a href={props.href}
+        <a
+          href={props.href}
           className={className.join(" ")}
           style={props.style}
-          target={props.target === "_blank" ? "_blank" : ""}
-          rel={props.target === "_blank" ? "noopener noreferrer" : ""}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
         >
           {props.children}
         </a>
-      )
+      );
     } else {
-      <Link
-        to={props.href}
-        className={className.join(" ")}
-        style={props.style}
-        onClick={onClick}
-      >
-        {props.children}
-      </Link>
+      return (
+        <Link
+          to={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      )
     }
   }
 
-  return <button
-    className={className.join(" ")}
-    style={props.style}
-    onClick={onClick}
-  >
-    {props.children}
-  </button>
+  return (
+    <button
+      className={className.join(" ")}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 Button.propTypes = {
   type: propTypes.oneOf(["button", "link"]),
   onClick: propTypes.func,
   href: propTypes.string,
+  target: propTypes.string,
   className: propTypes.string,
+  isPrimary: propTypes.bool,
+  isLight: propTypes.bool,
+  isExternal: propTypes.bool,
   isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
   isBlock: propTypes.bool,
-  isExternal: propTypes.bool,
   hasShadow: propTypes.bool,
 }
