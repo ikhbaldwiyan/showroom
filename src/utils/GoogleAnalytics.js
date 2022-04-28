b4fbd75e37c8e0b26ab2b5d602e0983dcea333ea
ikhbal <ikhbaldwiyan@gmail.com>
2022-04-28 17:25:45 +0700

    remove unused code in GA helper

--- src/utils/GoogleAnalytics.js
+++ src/utils/GoogleAnalytics.js
@@ -3,13 +3,11 @@ import { withRouter } from 'react-router-dom';
 import ReactGA from 'react-ga';
 
 const RouteChangeTracker = ({ history }) => {
-    console.log(history)
     const TRACKING_ID = 'UA-226891621-1';
     ReactGA.initialize(TRACKING_ID);
     
     useEffect(() => {
         history.listen((location, action) => {
-            ReactGA.set({ page: location.pathname });
             ReactGA.pageview(location.pathname);
         }, [history]);
     })
