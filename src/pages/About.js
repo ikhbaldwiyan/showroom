70569aa5fef9e8180dbea0cd7b779c2043b3bc89
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2022-10-15 18:30:22 +0700

    feat(about) update source code info and donation link

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,20 +16,35 @@ function About(props) {
           </p>
           <p>
             Website JKT48 Showroom ini di kembangkan oleh 
-            <a style={{color: '#24a2b7'}} href="https://twitter.com/inzoid" target="_blank"> <b>Inzoid</b> </a>
-            dan masih terus di develop sampe saat ini
+            <a style={{color: '#24a2b7'}} href="https://twitter.com/inzoid" rel="noreferrer" target="_blank"> <b>Inzoid</b> </a>
+            dan masih terus di develop sampe saat ini. <br />
+            Jika kalian ingin mendukung perkembangan project ini untuk masalah server dan lainnya <br />
+            kalian juga bisa donasi di link berikut <a href="https://saweria.co/Inzoid" rel="noreferrer" target="_blank">https://saweria.co/Inzoid</a>
           </p>
           <p>Source :</p>
           <ul>
             <li>
-              <a href="https://www.showroom-live.com/" target="_blank">
+              <a href="https://www.showroom-live.com/" rel="noreferrer" target="_blank">
                 https://www.showroom-live.com
               </a> (Official Showroom)
             </li>
             <li>
-              <a style={{wordBreak: 'break-word'}} href="https://qiita.com/takeru7584/items/f4ba4c31551204279ed2" target="_blank">
+              <a style={{wordBreak: 'break-word'}} rel="noreferrer" href="https://qiita.com/takeru7584/items/f4ba4c31551204279ed2" target="_blank">
                 https://qiita.com/takeru7584/items/f4ba4c31551204279ed2
               </a> (Showroom API)
+            </li>
+          </ul>
+          <p>Source Code :</p>
+          <ul>
+            <li>
+              <a href="https://github.com/ikhbaldwiyan/showroom" rel="noreferrer" target="_blank">
+                https://github.com/ikhbaldwiyan/showroom
+              </a> (JKT48 SHOWROOM WEB)
+            </li>
+            <li>
+              <a style={{wordBreak: 'break-word'}} rel="noreferrer" href="https://github.com/ikhbaldwiyan/jkt48-showroom-api" target="_blank">
+                https://github.com/ikhbaldwiyan/jkt48-showroom-api
+              </a> (JKT48 SHOWROOM API)
             </li>
           </ul>
         </div>
