bb348145a957103f6526c2b454aa9b024b4a9929
Ikhbal Dwiyantoro <ikhbaldwiyan@gmail.com>
2023-02-11 22:44:28 +0700

    add toast info for mobile

--- src/pages/layout/MainLayout.jsx
+++ src/pages/layout/MainLayout.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import { Container } from 'reactstrap';
+import React from "react";
+import { Container } from "reactstrap";
 
-import Header from 'parts/Header';
-import Footer from 'parts/Footer';
-import { isMobile } from 'react-device-detect';
+import Header from "parts/Header";
+import Footer from "parts/Footer";
+import { isMobile } from "react-device-detect";
 import { ToastContainer } from "react-toastify";
 
 function MainLayout(props) {
-  return (
-    isMobile ? (
-      <>
-        <Header {...props} />
-          {props.children}
-        <Footer />
-      </>
-    ) : (
-      <>
-          {props.isMultiRoom ? (
-            <>
-              <Header {...props} isMultiRoom/>
-              <Container fluid>
-                {props.children}
-              </Container>
-            </>
-          ): (
-            <>
-              <ToastContainer position="top-right" autoClose={3000} />
-              <Header {...props} />
-              <Container>
-                {props.children}
-              </Container>
-            </>
-          )}
-        <Footer />
-      </>
-    )
-  )
+  return isMobile ? (
+    <>
+      <Header {...props} />
+      {props.children}
+      <ToastContainer position="bottom-center" autoClose={3000} />
+      <Footer />
+    </>
+  ) : (
+    <>
+      {props.isMultiRoom ? (
+        <>
+          <Header {...props} isMultiRoom />
+          <Container fluid>
+            {props.children}
+            <ToastContainer position="top-right" autoClose={3000} />
+          </Container>
+        </>
+      ) : (
+        <>
+          <Header {...props} />
+          <Container>
+            {props.children}
+            <ToastContainer position="top-right" autoClose={3000} />
+          </Container>
+        </>
+      )}
+      <Footer />
+    </>
+  );
 }
 
 export default MainLayout;
