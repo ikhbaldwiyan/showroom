0e8871a2b9f859bd82d4e082268a0a9f30a6da4b
AldyRevigustian <aldyrevig@gmail.com>
2023-02-13 23:57:45 +0700

    farm

--- src/pages/Farm.js
+++ src/pages/Farm.js
@@ -1,10 +1,11 @@
 import { Container, Table } from "reactstrap";
 import MainLayout from "./layout/MainLayout";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Loading } from "components";
 import { FARM, GET_OFFICIAL } from "utils/api/api";
 import axios from "axios";
 import Button from "elements/Button";
+import { toast } from "react-toastify";
 
 function Farm(props) {
   const [cookiesLoginId, setCookiesLoginId] = useState("");
@@ -20,12 +21,15 @@ function Farm(props) {
   const [currentRoomId, setCurrentRoomId] = useState('');
   const [completedRoomIds, setCompletedRoomIds] = useState([]);
   const [allMessage, setAllMessage] = useState([]);
+  const [time, setTime] = useState(0);
 
+  const [limitUntil, setLimitUntil] = useState([]);
 
   useEffect(() => {
     const userSession = localStorage.getItem("session");
     const officialRoom = localStorage.getItem("official_room");
     const successRoom = localStorage.getItem("success_room");
+    const limited = localStorage.getItem("limit_until");
 
     if (userSession) {
       const foundSession = JSON.parse(userSession);
@@ -39,25 +43,51 @@ function Farm(props) {
     }
 
     if (successRoom) {
-      const foundSuccess = JSON.parse(successRoom);
+      const foundSuccess = JSON.parse(successRoom) || [];
       setSuccessRoom(foundSuccess);
+    }
+
+    if (limited) {
+      const foundLimit = JSON.parse(limited);
+      setLimitUntil(foundLimit);
+      let formatTime = foundLimit.replaceAll('.', '').split('after ')
+
+      let currentTime = new Date();
+      let currentDate = currentTime.toISOString().substr(0, 10);
+      let targetTime = new Date(currentDate + " " + formatTime[1] + ":00");
+
+      if (currentTime.getTime() > targetTime.getTime()) {
+        targetTime.setDate(targetTime.getDate() + 1);
+      }
+
+      // console.log(currentTime, currentTime.getTime());
+      // console.log(targetTime, targetTime.getTime());
+
+      let timeUntilTarget = targetTime.getTime() - currentTime.getTime();
+      console.log(timeUntilTarget);
+
+      setTimeout(() => {
+        localStorage.removeItem('official_room');
+        localStorage.removeItem('success_room');
+        localStorage.removeItem('limit_until');
+        console.log('deleted');
+      }, timeUntilTarget);
     }
 
   }, []);
 
+
   const textColor = (message) => {
     if (message.includes('Sukses')) {
       return 'text-success'
-    } else if(message.includes('Gagal')){
+    } else if (message.includes('Gagal')) {
       return 'text-danger'
-    } else if(message.includes('Sedang')){
+    } else if (message.includes('Sedang')) {
       return 'text-warning'
-    }else{
-      return 'text-light'
+    } else {
+      return 'text-secondary'
     }
   }
-
-
 
   const getOfficials = async () => {
     try {
@@ -74,13 +104,43 @@ function Farm(props) {
     }
   };
 
-
-  const test = async () => {
-    // setSuccessRoom(successRoom.push('HAHAH'));
-
+  const setLocalAndState = (roomId) => {
     var storedArray = localStorage.getItem('success_room');
     storedArray = JSON.parse(storedArray) || [];
 
+    storedArray.push(roomId);
+    storedArray = JSON.stringify(storedArray);
+    localStorage.setItem('success_room', storedArray);
+    setSuccessRoom(storedArray);
+  }
+
+  const deleteArray = () => {
+    let arrayLocal = localStorage.getItem("official_room");
+    arrayLocal = JSON.parse(arrayLocal) || [];
+
+    let updatedArray = [...arrayLocal];
+    updatedArray.shift();
+    setOfficialRoom(updatedArray);
+    localStorage.setItem("official_room", JSON.stringify(updatedArray));
+  };
+
+  const intervalId = useRef(null);
+
+  const decrementTime = () => {
+    clearInterval(intervalId.current);
+    setTime(50);
+    intervalId.current = setInterval(() => {
+      setTime(time => {
+        if (time > 0) {
+          return time - 1;
+        }
+        clearInterval(intervalId.current);
+        return 0;
+      });
+    }, 1000);
+  };
+
+  const startFarming = async () => {
     for (let i = 0; i < officialRoom.length; i++) {
       setLoading(true);
       const roomId = officialRoom[i];
@@ -97,6 +157,9 @@ function Farm(props) {
       setAllMessage(prevData => [...prevData, data.message]);
 
       if (data.message.includes('Sedang')) {
+        setTime(50)
+        decrementTime()
+
         await new Promise(resolve => setTimeout(resolve, 50 * 1000));
 
         const response2 = await axios.post(FARM, {
@@ -108,78 +171,162 @@ function Farm(props) {
         console.log(data2, 'SECOND');
 
         if (data2.message.includes('Sukses')) {
-          storedArray.push(roomId);
-          storedArray = JSON.stringify(storedArray);
-          localStorage.setItem('success_room', storedArray);
-          setSuccessRoom(storedArray);
+          deleteArray()
+          setLocalAndState(roomId)
+          toast.success(`Sukses Farm Di Room : ${roomId}`, {
+            theme: "colored"
+          });
         }
 
         if (data2.message.includes('Gagal')) {
+          deleteArray()
           setAllMessage(prevData => [...prevData, data2.message]);
+          toast.error(
+            data2.message.split(": ")[1] ??
+            "An error occured. Please go back and try again.",
+            {
+              theme: "colored"
+            }
+          );
+          localStorage.setItem('limit_until', JSON.stringify(data2.until));
+          setLimitUntil(data2.until)
           return;
+        }
+
+        if (data2.message.includes('Sedang')) {
+          deleteArray()
+        }
+
+        if (data2.message.includes('Offline')) {
+          deleteArray()
         }
 
         setAllMessage(prevData => [...prevData, data2.message]);
       }
 
       if (data.message.includes('Sukses')) {
-        storedArray.push(roomId);
-        storedArray = JSON.stringify(storedArray);
-        localStorage.setItem('success_room', storedArray);
-        setSuccessRoom(storedArray);
+        deleteArray()
+        setLocalAndState(roomId)
+        toast.success(`Sukses Farm Di Room : ${roomId}`, {
+          theme: "colored"
+        });
       }
 
       if (data.message.includes('Gagal')) {
+        deleteArray()
+        toast.error(
+          data.message.split(": ")[1] ??
+          "An error occured. Please go back and try again.",
+          {
+            theme: "colored"
+          }
+        );
+        localStorage.setItem('limit_until', JSON.stringify(data.until));
+        setLimitUntil(data.until)
         return;
       }
 
+      if (data.message.includes('Offline')) {
+        deleteArray()
+      }
       setCompletedRoomIds(prevRoomIds => [...prevRoomIds, roomId]);
 
       setProgress(prevProgress => prevProgress + (100 / officialRoom.length));
       setCurrentRoomId(null);
     }
-    // setLoading(false);
   };
+
+  const setWithExpiry = (key, value, ttl) => {
+    const now = new Date()
+
+    // `item` is an object which contains the original value
+    // as well as the time when it's supposed to expire
+    const item = {
+      value: value,
+      expiry: now.getTime() + ttl,
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+  }
+
+  const getWithExpiry = (key) => {
+    const itemStr = localStorage.getItem(key)
+    // if the item doesn't exist, return null
+    if (!itemStr) {
+      return null
+    }
+
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+    // compare the expiry time of the item with the current time
+    if (now.getTime() > item.expiry) {
+      // If the item is expired, delete the item from storage
+      // and return null
+      localStorage.removeItem(key)
+      return null
+    }
+    return item.value
+  }
 
   return (
     <MainLayout {...props} style={{ color: 'white' }}>
-      <Container>
-        <Button onClick={getOfficials} className="btn mb-5 text-light float-start" disabled={btnLoadingRoom ? true : false} style={{ backgroundColor: "#24a2b7" }}>
+
+      <div className="row mb-5 justify-content-between">
+        <Button onClick={getOfficials} className="btn text-light" disabled={btnLoadingRoom ? true : false} style={{ backgroundColor: "#24a2b7" }}>
           {btnLoadingRoom ? <Loading color="white" size={8} /> : "Fetch Room"}
         </Button>
-        <Button onClick={test} className="btn mb-5 text-light float-right" style={{ backgroundColor: "#24a2b7" }}>
+        <Button onClick={startFarming} className="btn text-light" style={{ backgroundColor: "#24a2b7" }}>
           RUN TOOLS
         </Button>
-        <div className="row">
-          <Table bordered className="col-4">
-            <thead style={{ backgroundColor: "#24a2b7", color: "white" }}>
-              <tr style={{ textAlign: "center" }}>
-                <th>ROOM ID</th>
-              </tr>
-            </thead>
-            <tbody style={{ textAlign: "center", color: props.theme === "dark" && "white" }}>
-              {officialRoom.map(
-                (room, idx) => (
-                  <tr key={idx}>
-                    <td>{room}</td>
-                  </tr>
-                )
-              )}
-            </tbody>
-          </Table>
+      </div>
 
-          <div className="col-8">
-            Sukses Room :
-            <p>
-              {successRoom}
-            </p>
-            {loading ?
-              (
-                <div>
-                  {currentRoomId ? (
-                    <p>Fetching data for roomId: {currentRoomId}</p>
-                  ) : null}
-                  <div style={{ width: "100%", height: "20px", background: "#ddd" }}>
+      {
+        limitUntil ? (
+          <div className="row mb-5 justify-content-center text-warning">
+            <h3>
+              {limitUntil}
+            </h3>
+          </div>
+        ) : ''
+      }
+
+
+      <div className="row">
+        <Table bordered className="col-3">
+          <thead style={{ backgroundColor: "#24a2b7", color: "white" }}>
+            <tr style={{ textAlign: "center" }}>
+              <th>LIST ROOM FARM</th>
+            </tr>
+          </thead>
+          <tbody style={{ textAlign: "center", color: props.theme === "dark" && "white" }}>
+            {officialRoom.map(
+              (room, idx) => (
+                <tr key={idx}>
+                  <td>{room}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </Table>
+
+        <div className="col-9">
+          Sukses farming di Room :
+          <p>
+            {/* {successRoom.join(", ")} */}
+            {JSON.stringify(successRoom).replaceAll(',', ',  ').replaceAll('"', '')}
+          </p>
+
+          Jumlah Sukes Melakukan Faming :
+          <p>
+            {successRoom.length}
+          </p>
+
+
+          {currentRoomId ? (
+            time == 0 ? '' : <p>Sedang farming di Room {currentRoomId} silahkan menunggu {time} detik</p>
+          ) : null}
+
+          <div>
+            {/* <div style={{ width: "100%", height: "20px", background: "#ddd" }}>
                     <div
                       style={{
                         width: `${progress}%`,
@@ -187,38 +334,33 @@ function Farm(props) {
                         background: "#4CAF50"
                       }}
                     />
-                  </div>
+                  </div> */}
 
-                  <div className="my-3">
-                    <p className="p-0 m-0">Completed roomIds:</p>
-                    <span>
-                      {completedRoomIds.join(", ")}
-                    </span>
-                  </div>
+            {/* <div className="my-3">
+                <p className="p-0 m-0">Telah farm di Room :</p>
+                <span>
+                  {completedRoomIds.join(", ")}
+                </span>
+              </div> */}
 
-                  <div className="">
-                    <p>Message:</p>
-                    {allMessage.map(
-                      (message, idx) => (
-                        <li key={idx}>
-                          <p className={textColor(message)}>
-                            {message}
-                          </p>
-                        </li>
-                      )
-                    )}
-                  </div>
+            <div className="">
+              <p>Status:</p>
+              <ul>
+                {allMessage.map(
+                  (message, idx) => (
+                    <li key={idx}>
+                      <p className={textColor(message)}>
+                        {message}
+                      </p>
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
 
-                </div>
-              ) : (
-                ''
-                // data.map(item => (
-                //   // <p>{item.message}</p>
-                // ))
-              )}
           </div>
         </div>
-      </Container>
+      </div>
     </MainLayout>
   );
 }
