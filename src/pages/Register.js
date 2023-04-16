10767a4926fc966cda5e19e20cae5e799f59b1b9
ikhbal <ikhbaldwiyan@gmail.com>
2023-04-16 14:15:12 +0700

    add error register validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Container } from "reactstrap";
+import { Container, Input } from "reactstrap";
 import { REGISTER } from "utils/api/api";
 import { gaEvent } from "utils/gaEvent";
 import MainLayout from "./layout/MainLayout";
@@ -16,7 +16,8 @@ const Register = (props) => {
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] =
+    useState(false);
   const [error, setError] = useState("");
   const [buttonLoading, setButtonLoading] = useState(false);
   const navigate = useHistory();
@@ -30,46 +31,53 @@ const Register = (props) => {
         password: password,
         password_confirm: passwordConfirmation,
         name: name,
-        avatar_id: 1,
+        avatar_id: 1
       });
 
-      if (response.data.captcha_url) {
-        setButtonLoading(false);
-      }
+      response.data.error && showToastError(response.data.error);
 
       if (response.data.status.ok) {
         setButtonLoading(false);
-        gaEvent("Register Screen", "Register Success", "Register")
+        gaEvent("Register Screen", "Register Success", "Register");
 
-        toast.info(`Register Success Please login ${response.data.profile.name}`, {
-          theme: "colored",
-          autoClose: 1800,
-        });
+        toast.info(
+          `Register Success Please login ${response.data.profile.name}`,
+          {
+            theme: "colored",
+            autoClose: 1800
+          }
+        );
 
         setTimeout(() => {
           navigate.push("/login");
         }, 2500);
       }
-
-      if (response.data.error.length > 0) {
-        setError(
-          response.data.error ??
-            "An error occured. Please go back and try again."
-        );
-        toast.error(
-          response.data.error ??
-            "An error occured. Please go back and try again.",
-          {
-            theme: "colored"
-          }
-        );
-      }
     } catch (err) {
-      console.log(error);
-      gaEvent("Register Screen", "Register Failed", "Register")
+      gaEvent("Register Screen", "Register Failed", "Register");
       setButtonLoading(false);
     }
   };
+
+  const showToastError = (error) => {
+    if (error === "Incorrect authentication password") {
+      setError(error);
+      toast.error("The password confirmation does not match.", {
+        theme: "colored"
+      });
+    }
+
+    if (error && error !== "Incorrect authentication password") {
+      setError(error ?? "An error occured. Please go back and try again.");
+      toast.error(error ?? "An error occured. Please go back and try again.", {
+        theme: "colored"
+      });
+    }
+  };
+
+  const isPasswordError =
+    error === "Incorrect authentication password"
+      ? "password-toggle-icon mr-4"
+      : "password-toggle-icon";
 
   return (
     <MainLayout {...props}>
@@ -82,18 +90,22 @@ const Register = (props) => {
             <h3 className="py-3">Register Showroom</h3>
             <form onSubmit={handleRegister}>
               <div className="row">
-                <div className="col-12 mb-4">
+                <div className="col-12">
                   <label>
                     <h6>Account ID</h6>
                   </label>
-                  <input
+                  <Input
                     type="text"
                     required
                     className="form-control"
                     placeholder="Account ID"
                     value={accountId}
                     onChange={(e) => setAccountId(e.target.value)}
+                    invalid={error === "This account ID cannot be used."}
                   />
+                  <p className="mt-3" style={{ color: "red" }}>
+                    {error === "This account ID cannot be used." && error}
+                  </p>
                 </div>
                 <div className="col-12 mb-4">
                   <label>
@@ -113,16 +125,17 @@ const Register = (props) => {
                     <h6>Password</h6>
                   </label>
                   <div className="input-with-icon">
-                    <input
+                    <Input
                       type={showPassword ? "text" : "password"}
                       required
                       className="form-control"
                       placeholder="Password"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
+                      invalid={error === "Incorrect authentication password"}
                     />
                     <span
-                      className="password-toggle-icon"
+                      className={isPasswordError}
                       onClick={() => setShowPassword(!showPassword)}
                     >
                       {showPassword ? (
@@ -138,17 +151,20 @@ const Register = (props) => {
                     <h6>Password Confirmation</h6>
                   </label>
                   <div className="input-with-icon">
-                    <input
+                    <Input
                       type={showPasswordConfirmation ? "text" : "password"}
                       required
                       className="form-control"
                       placeholder="Password"
                       value={passwordConfirmation}
                       onChange={(e) => setPasswordConfirmation(e.target.value)}
+                      invalid={error === "Incorrect authentication password"}
                     />
                     <span
-                      className="password-toggle-icon"
-                      onClick={() => setShowPasswordConfirmation(!showPasswordConfirmation)}
+                      className={isPasswordError}
+                      onClick={() =>
+                        setShowPasswordConfirmation(!showPasswordConfirmation)
+                      }
                     >
                       {showPasswordConfirmation ? (
                         <FaEyeSlash fill="black" />
@@ -166,7 +182,9 @@ const Register = (props) => {
                     className="btn btn-block text-light mt-5 mb-4 py-2"
                     style={{ backgroundColor: "#24a2b7" }}
                     disabled={buttonLoading ? true : false}
-                    onClick={() => gaEvent("Login Screen", "Login Button", "Login")}
+                    onClick={() =>
+                      gaEvent("Register Screen", "Register Button", "Register")
+                    }
                   >
                     {buttonLoading ? (
                       <Loading color="white" size={8} />
