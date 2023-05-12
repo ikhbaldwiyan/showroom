16024ee8a202eabce57086bbf6185a21e325c602
ikhbal <ikhbaldwiyan@gmail.com>
2023-05-12 19:59:17 +0700

    show new logo icon in header

--- src/components/AlertInfo.jsx
+++ src/components/AlertInfo.jsx
@@ -7,14 +7,14 @@ const AlertInfo = ({ page, label }) => {
   return (
     <UncontrolledAlert color="primary">
       <FaDiscord size="23px" className="mb-1 mr-2" />
-      Join grup Discord untuk update info live dan fitur baru
+      <span className="discord-text">Join grup Discord untuk update info live dan fitur baru</span>
       <a
         href={process.env.REACT_APP_DISCORD_LINK}
         target="_blank"
         rel="noreferrer"
         onClick={() => gaEvent(page, "Discord Link Click", label)}
       >
-        <b className="mx-1">
+        <b className="mx-1 discord-text">
           JOIN DISINI
         </b>
       </a>
