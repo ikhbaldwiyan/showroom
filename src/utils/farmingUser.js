076265d9b4c6f32f64941240d4fb25a34feec2be
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-05-18 17:23:22 +0700

    register farming user data

---
+++ src/utils/farmingUser.js
@@ -0,0 +1,34 @@
+import { getSession } from "./getSession";
+
+export const farmingUser = () => {
+  const accountId = getSession().user.account_id;
+
+  const registeredUsers = [
+    {
+      name: "Inzoid",
+      accountId: "inzoid"
+    },
+    {
+      name: "Ikhbal",
+      accountId: "dispatch"
+    },
+    {
+      name: "Lowly",
+      accountId: "inzoid21"
+    },
+    {
+      name: "Farming Account",
+      accountId: "akunsorum"
+    },
+  ];
+
+  const isRegisteredUser = registeredUsers.some(user => user.accountId === accountId);
+
+  if (isRegisteredUser) {
+    console.log("Access granted");
+    return true;
+  } else {
+    console.log("Access denied");
+    return false;
+  }
+};
