75a42606f9fd7f97212ea9e698a9342c0d8488e6
ikhbal <ikhbaldwiyan@gmail.com>
2023-06-24 21:36:18 +0700

    remove web socket closed reload

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -68,7 +68,6 @@ export default function Comment({ roomId, isMultiRoom, setRoomId, secretKey }) {
 
     newSocket.addEventListener('close', () => {
       console.log('WebSocket closed');
-      !isMultiRoom ? window.location.reload() : setRoomId(roomId);
     });
 
     setSocket(newSocket);
