be0c78ec3cf080f13ed48f1e6991225d740d1354
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-08-04 15:52:10 +0700

    revamp theater schedule design

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
-import Button from "elements/Button";
 import moment from "moment";
-import SkeletonLive from "parts/skeleton/SkeletonLive";
+// import SkeletonLive from "parts/skeleton/SkeletonLive";
 import React, { useEffect, useState } from "react";
-import { isMobile } from "react-device-detect";
+import { FaCalendarAlt, FaRegClock } from "react-icons/fa";
 import { Fade } from "react-reveal";
+import { Col, Row } from "reactstrap";
 import { SCHEDULES_API } from "utils/api/api";
+import "moment/locale/id";
 import { slugify } from "utils/slugify";
+import { Link } from "react-router-dom";
 
 const Schedule = ({ theme, isSearch }) => {
   const [schedule, setSchedule] = useState([]);
@@ -23,51 +25,62 @@ const Schedule = ({ theme, isSearch }) => {
 
   return (
     !isSearch && (
-      <>
-        <h3 className="py-2 theater-title">Jadwal Theater</h3>
-        <div className="container-grid">
-          {schedule.length ? (
+      <Fade>
+        <h3 className="py-1 font-weight-bold">Jadwal Theater</h3>
+        <Row>
+          {schedule.length &&
             schedule.map((item, idx) => (
-                <div
-                  key={idx}
-                  className={`item ${isMobile ? "column-12" : "column-3"} row-1`}
-                >
-                  <Fade bottom>
-                    <div className="card card-featured">
-                      <Button
-                        href={`/theater/${slugify(item?.setlist?.name)}/${item?._id}`}
-                        type="link"
-                      >
-                        <div className="tag" style={{ backgroundColor: "teal" }}>
-                          {moment(item?.showDate).format("DD MMMM")}
-                        </div>
-                        <figure className="img-wrapper">
-                          <img
-                            src={item?.setlist?.image}
-                            alt={item?.setlist?.name}
-                            className="img-cover"
-                          />
-                        </figure>
-                        <div className="meta-wrapper">
-                          <Button
-                            type="link"
-                            style={{ textDecoration: "none" }}
-                            className="strecthed-link d-block text-white"
-                            href={`/theater/${slugify(item?.setlist?.name)}/${item?._id}`}
-                          >
-                            <h5>{item?.setlist?.name}</h5>
-                          </Button>
-                        </div>
-                      </Button>
+              <Col key={idx} className="mb-4" md="4" sm="12">
+                <h4 className="py-2">
+                  <b>{item?.setlist?.name}</b>
+                </h4>
+                <Row>
+                  <Col md="8">
+                    <div className="theater-date">
+                      <div className="d-flex align-items-center">
+                        <FaCalendarAlt size={18} className="mr-2" />
+                        {moment(item.showDate)
+                          .locale("id")
+                          .format("dddd, DD MMMM YYYY")}
+                      </div>
                     </div>
-                  </Fade>
+                  </Col>
+                  <Col md="4">
+                    <div className="theater-schedule-time">
+                      <div className="d-flex align-items-center">
+                        <FaRegClock size={18} className="mr-2" />
+                        {item?.showTime}
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+                <img
+                  width="100%"
+                  src={item?.setlist?.image}
+                  alt={item?.setlist?.name}
+                  style={{
+                    maxHeight: "215px",
+                    objectFit: "cover",
+                    borderRadius: "6px 6px 0px 0px",
+                  }}
+                />
+                <div className="card-schedule">
+                  <div className="card-desc">
+                    {item?.setlist?.description &&
+                    item.setlist.description.length > 160
+                      ? item.setlist.description.slice(0, 160) + "..."
+                      : item.setlist.description}
+                  </div>
                 </div>
-              ))
-          ) : (
-            <SkeletonLive theme={theme} />
-          )}
-        </div>
-      </>
+                <Link
+                  to={`/theater/${slugify(item?.setlist?.name)}/${item?._id}`}
+                >
+                  <button className="theater-button">Detail Theater</button>
+                </Link>
+              </Col>
+            ))}
+        </Row>
+      </Fade>
     )
   );
 };
