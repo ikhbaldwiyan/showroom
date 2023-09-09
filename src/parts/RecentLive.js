f1cd595b870a0d9d033a9837aec2df0cfe145fae
ikhbal <ikhbaldwiyan@gmail.com>
2023-09-09 21:59:25 +0700

    adjust recent live card official room

--- src/parts/RecentLive.js
+++ src/parts/RecentLive.js
@@ -9,7 +9,7 @@ import {
   FaArrowRight,
   FaCalendarAlt,
   FaClock,
-  FaUserFriends,
+  FaUserFriends
 } from "react-icons/fa";
 import { RiHistoryFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
@@ -52,80 +52,106 @@ const RecentLive = ({ isSearch }) => {
     window.document.title = "Member Live History";
   }, []);
 
-  return !isSearch && (
-    <div className="mb-4">
-      <div className="d-flex align-items-center justify-content-between py-3">
-        <h3>Recent Live</h3>
-        <Link to="/live-history">
-          <div
-            className="d-flex align-items-center"
-            style={{ color: "#ECFAFC" }}
-          >
-            <FaArrowRight className="mb-2 mr-2" size={25} />
-            <h5>See More</h5>
-          </div>
-        </Link>
-      </div>
+  return (
+    !isSearch && (
+      <div className="mb-4">
+        <div className="d-flex align-items-center justify-content-between py-3">
+          <h3>Recent Live</h3>
+          <Link to="/live-history">
+            <div
+              className="d-flex align-items-center"
+              style={{ color: "#ECFAFC" }}
+            >
+              <FaArrowRight className="mb-2 mr-2" size={25} />
+              <h5>See More</h5>
+            </div>
+          </Link>
+        </div>
 
-      <Row>
-        {logs.map((log, idx) => {
-          const { member, live_info } = log;
-          return (
-            <Col key={idx} md="4" sm="12" className={`${idx !== 2 && "mb-3"}`}>
-              <div className="card-recent-live">
-                <img className="recent-image" src={member?.img_alt} alt="" />
-                <div className="recent-info-wrapper">
-                  <div className="recent-icon">
-                    <FaCalendarAlt size={18} />
-                    <span className="font-weight-bold">
-                      {moment(live_info?.date?.start).format("dddd, D MMMM")}
-                    </span>
-                  </div>
-                  <div className="recent-icon">
-                    <FaUserFriends size={18} />
-                    {formatNumber(live_info?.viewers)} Views
-                  </div>
-                  <div className="recent-icon">
-                    <FaClock size={18} />
-                    <span>{getLiveDuration(live_info?.duration)}</span>
-                  </div>
-                  <div className="recent-icon">
-                    <AiFillGift size={18} />
-                    {formatViews(log.points)} Gold
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-6">
-                  <Link to={`/history/${member.url}/${log.data_id}`}>
-                    <div className="recent-card-name">
-                      <span className="recent-name">
-                        {member?.url?.replace("JKT48_", "") + " JKT48"}
-                        <BiLogInCircle size={20} />
+        <Row>
+          {logs.map((log, idx) => {
+            const { member, live_info } = log;
+            return (
+              <Col
+                key={idx}
+                md="4"
+                sm="12"
+                className={`${idx !== 2 && "mb-3"}`}
+              >
+                <div className="card-recent-live">
+                  <img
+                    className="recent-image"
+                    src={
+                      member?.url === "officialJKT48"
+                        ? "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/JKT48.svg/1200px-JKT48.svg.png"
+                        : member?.img_alt
+                    }
+                    alt={member?.name}
+                  />
+                  <div className="recent-info-wrapper">
+                    <div className="recent-icon">
+                      <FaCalendarAlt size={18} />
+                      <span className="font-weight-bold">
+                        {moment(live_info?.date?.start).format("dddd, D MMMM")}
                       </span>
                     </div>
-                  </Link>
-                </div>
-                <div className="col-6">
-                  <div className="last-live">
-                    <RiHistoryFill color="#ECFAFC" size={20} className="mr-1" />
-                    <ReactTimeago
-                      style={{
-                        color: "#ECFAFC",
-                        fontWeight: "600",
-                        marginBottom: "1px",
-                      }}
-                      date={live_info.date.end}
-                    />
+                    <div className="recent-icon">
+                      <FaUserFriends size={18} />
+                      {formatNumber(live_info?.viewers)} Views
+                    </div>
+                    <div className="recent-icon">
+                      <FaClock size={18} />
+                      <span>{getLiveDuration(live_info?.duration)}</span>
+                    </div>
+                    <div className="recent-icon">
+                      <AiFillGift size={18} />
+                      {formatViews(log.points)} Gold
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Col>
-          );
-        })}
-      </Row>
-      <hr style={{ borderColor: "white" }} />
-    </div>
+                <div className="row mt-1">
+                  <div className="col-6">
+                    <Link to={`/history/${member.url}/${log.data_id}`}>
+                      <div className="recent-card-name">
+                        {member?.url === "officialJKT48" ? (
+                          <span className="recent-name">
+                            JKT48 Official
+                            <BiLogInCircle size={20} />
+                          </span>
+                        ) : (
+                          <span className="recent-name">
+                            {member?.url?.replace("JKT48_", "") + " JKT48"}
+                            <BiLogInCircle size={20} />
+                          </span>
+                        )}
+                      </div>
+                    </Link>
+                  </div>
+                  <div className="col-6">
+                    <div className="last-live">
+                      <RiHistoryFill
+                        color="#ECFAFC"
+                        size={20}
+                        className="mr-1"
+                      />
+                      <ReactTimeago
+                        style={{
+                          color: "#ECFAFC",
+                          fontWeight: "600",
+                          marginBottom: "1px"
+                        }}
+                        date={live_info.date.end}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+        <hr style={{ borderColor: "white" }} />
+      </div>
+    )
   );
 };
 
