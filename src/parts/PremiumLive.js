942ab67436fe454c78780602f00d50d51d019dd6
ikhbal <ikhbaldwiyan@gmail.com>
2023-09-10 17:35:16 +0700

    adjust premium live image cover and title to today show

--- src/parts/PremiumLive.js
+++ src/parts/PremiumLive.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Button from "elements/Button";
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { isMobile } from "react-device-detect";
 import { FaUser } from "react-icons/fa";
@@ -11,9 +12,9 @@ import { Link } from "react-router-dom";
 import {
   getRoomPremiumLiveFailed,
   getRoomPremiumLiveLoad,
-  getRoomPremiumLiveSuccess,
+  getRoomPremiumLiveSuccess
 } from "redux/actions/roomLives";
-import { ROOM_LIVES_API } from "utils/api/api";
+import { ROOM_LIVES_API, TODAY_SCHEDULE_API } from "utils/api/api";
 import formatViews from "utils/formatViews";
 import getTimes from "utils/getTimes";
 import SkeletonLive from "./skeleton/SkeletonLive";
@@ -23,6 +24,7 @@ const PremiumLive = ({ theme }) => {
     (state) => state.roomLives
   );
   const dispatch = useDispatch();
+  const [theater, setTheater] = useState({});
 
   useEffect(() => {
     dispatch(getRoomPremiumLiveLoad());
@@ -41,34 +43,46 @@ const PremiumLive = ({ theme }) => {
     getPremiumLive();
   }, []);
 
+  useEffect(() => {
+    const getTodayTheater = () => {
+      axios.get(TODAY_SCHEDULE_API).then((res) => {
+        setTheater(res.data);
+      });
+    };
+    getTodayTheater();
+  }, []);
+
   return isPremiumLive ? (
-    <div className="mb-2">
-      <h3 className="my-3"> {isLoading && "Loading"} Premium Live </h3>
-      {isLoading && !isMobile ? (
-        <SkeletonLive theme={theme} liveLength={premium_live.length} />
-      ) : premium_live.length !== 0 ? (
-        <div className="container-grid">
-          {premium_live.map((item, idx) => (
-            <div
-              key={idx}
-              className={`item ${
-                isMobile ? "column-12 row-1" : `column-4 row-1`
-              }`}
-            >
-              <Link
-                to={`room/${item.url_key ?? item.room_url_key}/${
-                  item.id ?? item.room_id
+    <Fade bottom>
+      <div className="mb-2">
+        <h3 className="my-3"> {isLoading && "Loading"} Premium Live </h3>
+        {isLoading && !isMobile ? (
+          <SkeletonLive theme={theme} liveLength={premium_live.length} />
+        ) : premium_live.length !== 0 ? (
+          <div className="container-grid">
+            {premium_live.map((item, idx) => (
+              <div
+                key={idx}
+                className={`item ${
+                  isMobile ? "column-12 row-1" : `column-4 row-1`
                 }`}
               >
-                <div className="card card-featured">
-                  <Fade right>
+                <Link
+                  to={`room/${item.url_key ?? item.room_url_key}/${
+                    item.id ?? item.room_id
+                  }`}
+                >
+                  <div className="card card-featured">
                     <div className="tag">
                       <FaUser style={{ width: "10" }} className="mb-1" />{" "}
                       {formatViews(item.view_num)}
                     </div>
                     <figure className="img-wrapper">
                       <img
-                        src={item.image.replace("s.png", "l.png")}
+                        src={
+                          theater?.setlist?.image ??
+                          item.image.replace("s.png", "l.png")
+                        }
                         alt={item.room_name}
                         className="img-cover"
                       />
@@ -83,30 +97,35 @@ const PremiumLive = ({ theme }) => {
                         }`}
                       >
                         <h5 className="d-inline">
-                          {item.main_name}{" "}
+                          {theater
+                            ? theater?.setlist?.originalName +
+                              " - " +
+                              theater?.setlist?.name +
+                              " "
+                            : item?.main_name}
                         </h5>
                         <h6 className="d-inline" style={{ color: "#ced4da" }}>
                           {getTimes(item.started_at)}
                         </h6>
                       </Button>
                     </div>
-                  </Fade>
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div className="container">
-          <div className="row">
-            <div className="col-12 mt-5 text-center">
-              <IoVideocamOff size={100} />
-              <h3 className="mt-3">Room Live Not Found</h3>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="container">
+            <div className="row">
+              <div className="col-12 mt-5 text-center">
+                <IoVideocamOff size={100} />
+                <h3 className="mt-3">Room Live Not Found</h3>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </Fade>
   ) : (
     ""
   );
