4ac95e47eb3b474072ca22d6f165a80d5c28739e
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-12-01 16:03:29 +0700

    implement download  wrapped page screenshot

--- src/pages/wrapper/ShowroomWrapped.js
+++ src/pages/wrapper/ShowroomWrapped.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import { Loading } from "components";
 import Sidebar from "pages/layout/Sidebar";
 import React, { useEffect, useState } from "react";
-import { isMobile } from "react-device-detect";
 import { BsCollectionPlayFill } from "react-icons/bs";
-import { FaMoneyBillWave, FaTheaterMasks, FaVideoSlash } from "react-icons/fa";
+import {
+  FaDownload,
+  FaMoneyBillWave,
+  FaTheaterMasks,
+  FaVideoSlash,
+} from "react-icons/fa";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { Col, Row } from "reactstrap";
+import { Button, Col, Row } from "reactstrap";
 import { MOST_WATCH, PREMIUM_LIVES } from "utils/api/api";
 import formatNumber from "utils/formatNumber";
 import { getSession } from "utils/getSession";
@@ -14,6 +18,7 @@ import { showToast } from "utils/showToast";
 import useWindowDimensions from "utils/useWindowDimension";
 import Logo from "../../../src/assets/images/logo-dark.svg";
 import Header from "./Header";
+import html2canvas from "html2canvas";
 
 const ShowroomWrapped = () => {
   const [mostWatch, setMostWatch] = useState([]);
@@ -56,6 +61,35 @@ const ShowroomWrapped = () => {
     }
   }, []);
 
+  const takeScreenshot = () => {
+    const targetElement = document.getElementById("screenshotTarget"); // Replace 'screenshotTarget' with the appropriate ID of the element you want to capture
+    if (targetElement) {
+      const originalBackgroundColor = targetElement.style.backgroundColor;
+      targetElement.style.backgroundColor = "#282c34"; // Change background color to transparent
+
+      // Wait for images to load before taking the screenshot
+      html2canvas(targetElement, {
+        allowTaint: true,
+        useCORS: true,
+        scrollX: 0,
+        scrollY: 0,
+        windowWidth: 400,
+        windowHeight: 900,
+      }).then((canvas) => {
+        const screenshotImage = canvas.toDataURL("image/png");
+
+        // Restore the original background color
+        targetElement.style.backgroundColor = originalBackgroundColor;
+
+        // Create a link for the user to download the screenshot
+        const downloadLink = document.createElement("a");
+        downloadLink.href = screenshotImage;
+        downloadLink.download = "jkt48-showroom-wrapped.png";
+        downloadLink.click();
+      });
+    }
+  };
+
   return (
     <Row className="px-3">
       {width > 768 && (
@@ -64,155 +98,169 @@ const ShowroomWrapped = () => {
         </Col>
       )}
       <Col className="p-0" sm="12" md="10">
-        <Header />
-        <div className={`layout ${!isMobile && "mx-3"}`}>
-          {getSession().session && (
-            <Row>
-              <Col className="d-flex" md="12">
-                <div className="user">
-                  <div className="user-wrapper">
-                    <div>
-                      <img className="user-image" src={profile?.image} alt="" />
-                    </div>
-                    <div>
-                      <b className="username">{profile?.name}</b> <br />
-                      <span>ID: {user?.account_id}</span>
-                    </div>
-                    <div>
+        <div id="screenshotTarget">
+          <Header />
+          <div className="layout wrapped-container">
+            {getSession().session && (
+              <Row>
+                <Col className="d-flex">
+                  <div className="user">
+                    <div className="user-wrapper">
                       <div>
                         <img
                           className="user-image"
-                          src={profile?.avatar_url}
-                          alt="avatar"
+                          src={profile?.image}
+                          alt=""
                         />
                       </div>
+                      <div>
+                        <b className="username">{profile?.name}</b> <br />
+                        <span>ID: {user?.account_id}</span>
+                      </div>
+                      <div>
+                        <div>
+                          <img
+                            className="user-image"
+                            src={profile?.avatar_url}
+                            alt="avatar"
+                          />
+                        </div>
+                      </div>
                     </div>
+                  </div>
+                </Col>
+              </Row>
+            )}
+            <Row className="py-3">
+              <Col sm="12" md="5">
+                <div className="wrapper-container">
+                  <div className="d-flex mb-2">
+                    <BsCollectionPlayFill className="mr-2" size={23} />
+                    <h5 className="wrapper-title">Most Watch Showroom</h5>
+                  </div>
+                  <div className="d-flex">
+                    {mostWatch[0]?.image ? (
+                      <img
+                        className="img-top"
+                        src={mostWatch[0]?.image}
+                        alt="member"
+                      />
+                    ) : (
+                      <img src={Logo} alt="logo" width={50} />
+                    )}
+                    <ol className="top-member-wrap">
+                      {mostWatch.length > 0 ? (
+                        mostWatch?.slice(0, 3).map((item, idx) => (
+                          <>
+                            <li>
+                              {item.name} - <b>{item.visit}x</b>
+                            </li>
+                          </>
+                        ))
+                      ) : (
+                        <Loading />
+                      )}
+                    </ol>
                   </div>
                 </div>
               </Col>
             </Row>
-          )}
-          <Row className="py-3">
-            <Col sm="12" md="5">
-              <div className="wrapper-container">
-                <div className="d-flex mb-2">
-                  <BsCollectionPlayFill className="mr-2" size={23} />
-                  <h5 className="wrapper-title">Most Watch Showroom</h5>
-                </div>
-                <div className="d-flex">
-                  {mostWatch[0]?.image ? (
-                    <img
-                      className="img-top"
-                      src={mostWatch[0]?.image}
-                      alt="member"
-                    />
-                  ) : (
-                    <img src={Logo} alt="logo" width={50} />
+            <Row className="mb-3">
+              <Col sm="12" md="5">
+                <div className="wrapper-container">
+                  <div className="d-flex mb-2">
+                    <FaTheaterMasks className="mr-2" size={23} />
+                    <h5 className="wrapper-title">Top Premium Live Setlist</h5>
+                  </div>
+                  <div className="setlist-wrapped">
+                    {isLoading ? (
+                      <img src={Logo} alt="logo" width={50} />
+                    ) : premiumLives?.topSetlist !==
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/JKT48.svg/1200px-JKT48.svg.png" ? (
+                      <img
+                        className="img-setlist"
+                        src={premiumLives?.topSetlist}
+                        alt=""
+                      />
+                    ) : (
+                      ""
+                    )}
+                    <ol
+                      style={{ paddingLeft: "30px" }}
+                      className="top-setlist-wrap"
+                    >
+                      {premiumLives?.show?.length > 0 ? (
+                        premiumLives?.show?.slice(0, 3)?.map((item, idx) => (
+                          <li key={idx}>
+                            {item?.name} - <b>{item?.total}x</b>
+                          </li>
+                        ))
+                      ) : isLoading ? (
+                        <Loading />
+                      ) : (
+                        <div className="d-flex flex-column align-items-center justify-content-center justify-items-center">
+                          <FaVideoSlash size={40} />
+                          <span className="text-sm">
+                            Premium Live History not found
+                          </span>
+                        </div>
+                      )}
+                    </ol>
+                  </div>
+                  {premiumLives?.totalPaidLive !== 0 && !isLoading && (
+                    <div className="total-paid-live">
+                      <span>
+                        Total Pembelian: {premiumLives?.totalPaidLive}x
+                      </span>
+                    </div>
                   )}
-                  <ol className="top-member-wrap">
-                    {mostWatch.length > 0 ? (
-                      mostWatch?.slice(0, 3).map((item, idx) => (
+                </div>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col sm="12" md="5">
+                <div className="money-container mb-2">
+                  <div className="d-flex mb-1">
+                    <FaMoneyBillWave className="mr-2" size={24} />
+                    <h5 className="wrapper-title">Money Spend</h5>
+                  </div>
+                  <div className="setlist-wrapped">
+                    <ul className="top-setlist-wrap money-spend">
+                      {isLoading ? (
                         <>
                           <li>
-                            {item.name} - <b>{item.visit}x</b>
+                            Total JPY: <Loading />
+                          </li>
+                          <li>
+                            Total IDR: <Loading />
                           </li>
                         </>
-                      ))
-                    ) : (
-                      <Loading />
-                    )}
-                  </ol>
-                </div>
-              </div>
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col sm="12" md="5">
-              <div className="wrapper-container">
-                <div className="d-flex mb-2">
-                  <FaTheaterMasks className="mr-2" size={23} />
-                  <h5 className="wrapper-title">
-                    Top Premium Live Setlist 2023
-                  </h5>
-                </div>
-                <div className="setlist-wrapped">
-                  {isLoading ? (
-                    <img src={Logo} alt="logo" width={50} />
-                  ) : premiumLives?.topSetlist !==
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/JKT48.svg/1200px-JKT48.svg.png" ? (
-                    <img
-                      className="img-setlist"
-                      src={premiumLives?.topSetlist}
-                      alt=""
-                    />
-                  ) : (
-                    ""
-                  )}
-                  <ol
-                    style={{ paddingLeft: "30px" }}
-                    className="top-setlist-wrap"
-                  >
-                    {premiumLives?.show?.length > 0 ? (
-                      premiumLives?.show?.slice(0, 3)?.map((item, idx) => (
-                        <li key={idx}>
-                          {item?.name} - <b>{item?.total}x</b>
-                        </li>
-                      ))
-                    ) : isLoading ? (
-                      <Loading />
-                    ) : (
-                      <div className="d-flex flex-column align-items-center justify-content-center justify-items-center">
-                        <FaVideoSlash size={40} />
-                        <span className="text-sm">
-                          Premium Live History not found
-                        </span>
-                      </div>
-                    )}
-                  </ol>
-                </div>
-                {premiumLives?.totalPaidLive !== 0 && !isLoading && (
-                  <div className="total-paid-live">
-                    <span>Total Pembelian: {premiumLives?.totalPaidLive}x</span>
+                      ) : (
+                        <>
+                          <li>
+                            Total JPY:{" "}
+                            <b>{formatNumber(premiumLives?.totalJPY)} JPY</b>
+                          </li>
+                          <li>
+                            Total IDR: <b>{premiumLives?.totalIDR}</b>
+                          </li>
+                        </>
+                      )}
+                    </ul>
                   </div>
-                )}
-              </div>
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col sm="12" md="5">
-              <div className="money-container">
-                <div className="d-flex mb-1">
-                  <FaMoneyBillWave className="mr-2" size={24} />
-                  <h5 className="wrapper-title">Money Spend</h5>
                 </div>
-                <div className="setlist-wrapped">
-                  <ul className="top-setlist-wrap money-spend">
-                    {isLoading ? (
-                      <>
-                        <li>
-                          Total JPY: <Loading />
-                        </li>
-                        <li>
-                          Total IDR: <Loading />
-                        </li>
-                      </>
-                    ) : (
-                      <>
-                        <li>
-                          Total JPY:{" "}
-                          <b>{formatNumber(premiumLives?.totalJPY)} JPY</b>
-                        </li>
-                        <li>
-                          Total IDR: <b>{premiumLives?.totalIDR}</b>
-                        </li>
-                      </>
-                    )}
-                  </ul>
-                </div>
-              </div>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          </div>
         </div>
+        <Col>
+          <Button color="primary" onClick={takeScreenshot}>
+            <div className="d-flex align-items-center">
+              <FaDownload className="mr-2" />
+              Download
+            </div>
+          </Button>
+        </Col>
       </Col>
     </Row>
   );
