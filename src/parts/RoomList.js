0f79c3699dceacdeed34a9cad889c0b8c4f1e104
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-12-06 15:42:18 +0700

    adjust responsive room list

--- src/parts/RoomList.js
+++ src/parts/RoomList.js
@@ -3,8 +3,12 @@ import Room from "components/Room";
 import SkeletonList from "./skeleton/SkeletonList";
 import { isMobile } from "react-device-detect";
 import { MdOutlineSearchOff } from "react-icons/md";
+import useWindowDimensions from "utils/useWindowDimension";
 
 function RoomList({ room, theme, isSearch, isSearchAcademy, isRoomFollowed }) {
+  const { width } = useWindowDimensions();
+  const style = width > 1500 ? "column-3" : "column-4";
+
   return (
     <>
       {isRoomFollowed ? (
@@ -21,7 +25,7 @@ function RoomList({ room, theme, isSearch, isSearchAcademy, isRoomFollowed }) {
       {room && room.length !== 0 ? (
         <div className="container-grid">
           {room.map((item, idx) => (
-            <Room idx={idx} item={item} style="column-4">
+            <Room idx={idx} item={item} style={style}>
               {item.is_live && (
                 <div className="tag" style={{ backgroundColor: "#CC2636" }}>
                   On Live
@@ -39,10 +43,12 @@ function RoomList({ room, theme, isSearch, isSearchAcademy, isRoomFollowed }) {
             </p>
           </div>
         )
-      ) : !isMobile && (
-        <div className="container-grid">
-          <SkeletonList theme={theme} />
-        </div>
+      ) : (
+        !isMobile && (
+          <div className="container-grid">
+            <SkeletonList theme={theme} />
+          </div>
+        )
       )}
     </>
   );
