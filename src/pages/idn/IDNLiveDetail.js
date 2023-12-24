f7dce6563345462133de40022f4d729d9b7b37e4
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-12-25 00:24:48 +0700

    :art: update UI video player and redirect to web orgiinal

--- src/pages/idn/IDNLiveDetail.js
+++ src/pages/idn/IDNLiveDetail.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 import MainLayout from "pages/layout/MainLayout";
-import Streaming from "pages/streaming/Stream";
 import { useState, useEffect } from "react";
-import { FaUsers } from "react-icons/fa";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { Col, Row } from "reactstrap";
 import { ROOM_LIVE_IDN_DETAIL } from "utils/api/api";
 import formatNumber from "utils/formatNumber";
-import LiveButton from "elements/Button";
 import RoomListIDN from "./components/RoomListIDN";
+import Player from "./components/Player";
 
 const IDNLiveDetail = () => {
   const [live, setLive] = useState("");
@@ -16,9 +14,7 @@ const IDNLiveDetail = () => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [id])
 
-  useEffect(() => {
     try {
       axios.get(ROOM_LIVE_IDN_DETAIL(id)).then((res) => {
         setLive(res.data);
@@ -33,17 +29,15 @@ const IDNLiveDetail = () => {
       <div className="layout">
         <Row>
           <Col md="8">
-            <Streaming url={live?.stream_url} />
+            <Player
+              url={live?.stream_url}
+              views={formatNumber(live?.view_count ?? 0)}
+              idnUrl={`https://www.idn.app/${id}/live/${live.slug}`}
+            />
             <div className="d-flex mb-3">
               <h4 className="mr-2">
                 <b>{live?.user?.name}</b> | {live?.title}
               </h4>
-              <LiveButton  className="btn-sm btn-danger">
-                <div className="d-flex align-items-center">
-                  <FaUsers size={20} className="mr-1" />
-                  {formatNumber(live?.view_count ?? 0)}
-                </div>
-              </LiveButton>
             </div>
           </Col>
           <Col>
---
+++ src/pages/idn/components/Player.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import "../../streaming/video.scss";
+import { HiUsers } from "react-icons/hi";
+
+export default function Player({ url, views, idnUrl }) {
+  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+  return (
+    <div className="idn-live player-wrapper mb-3">
+      <div className="logo">
+        <a href={idnUrl} target="_blank" rel="noreferrer">
+          <img
+            className="mt-1"
+            width={100}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/IDN_Live.svg/1024px-IDN_Live.svg.png"
+            alt="idn live"
+          />
+        </a>
+      </div>
+      <div className="views">
+        <div className="d-flex align-items-center">
+          <HiUsers size={20} className="mr-1" />
+          <b>{views}</b>
+        </div>
+      </div>
+      <ReactPlayer
+        className="react-player"
+        config={{
+          file: {
+            forceHLS: !isSafari,
+          },
+        }}
+        controls
+        url={url}
+        width="100%"
+        height="100%"
+        playing={true}
+      />
+    </div>
+  );
+}
