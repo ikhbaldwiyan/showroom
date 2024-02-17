b7c279f52b404a75f6079636d36ca28c788da1c2
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2024-02-18 01:07:45 +0700

    update ssl domain to https

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,5 +1,5 @@
 // BASE URL API
-const API = "http://jkt48showroom-api.my.id/api";
+const API = "https://jkt48showroom-api.my.id/api";
 const ADMIN_API = "https://showroom-admin.vercel.app"
 const BOT_API = "https://jkt48-showroom-bot.ikhbaldwiyan.repl.co/discord"
 const LIVE = `${API}/lives`;
