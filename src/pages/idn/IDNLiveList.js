27e94da27a53529d44dc83ed82a8612bed30d6a9
ikhbal <ikhbaldwiyan@gmail.com>
2024-05-08 21:59:41 +0700

    show idn live stream menu

--- src/pages/idn/IDNLiveList.js
+++ src/pages/idn/IDNLiveList.js
@@ -15,7 +15,9 @@ const IDNLiveList = () => {
   useEffect(() => {
     try {
       axios.get(ROOM_LIVES_IDN).then((res) => {
-        const filterLive = res.data.filter((item) => item.user.name.includes("JKT48"));
+        const filterLive = res.data.filter((item) =>
+          item.user.name.includes("JKT48")
+        );
         setRoomLives(filterLive);
       });
     } catch (error) {
@@ -35,43 +37,41 @@ const IDNLiveList = () => {
                 isMobile ? "column-12 row-1" : `column-3 row-1`
               }`}
             >
-              {/* <Link
+              <Link
                 to={{
                   pathname: `idn/${item.user.username}`,
                   state: {
-                   liveData: item,
-                  },
+                    liveData: item
+                  }
                 }}
-              > */}
-                <a href={`https://www.idn.app/${item.user.username}/live/${item.slug}`} target="_blank">
-                  <div className="card card-featured">
-                    <Fade right>
-                      <div className="tag">
-                        <FaUser size={15} /> {formatViews(item.view_count)}
-                      </div>
-                      <figure className="img-wrapper">
-                        <img
-                          src={item.user.avatar}
-                          alt={item.user.name}
-                          className="img-cover"
-                        />
-                      </figure>
-                      <div className="meta-wrapper">
-                        <Button
-                          type="link"
-                          style={{ textDecoration: "none" }}
-                          className="d-block text-white"
-                        >
-                          <h5 className="d-inline mr-2">{item.user.name}</h5>
-                          <h6 className="d-inline" style={{ color: "#ced4da" }}>
-                            {formatLongDate(item.live_at, true)}
-                          </h6>
-                        </Button>
-                      </div>
-                    </Fade>
-                  </div>
-                </a>
-              {/* </Link> */}
+              >
+                <div className="card card-featured">
+                  <Fade right>
+                    <div className="tag">
+                      <FaUser size={15} /> {formatViews(item.view_count)}
+                    </div>
+                    <figure className="img-wrapper">
+                      <img
+                        src={item.user.avatar}
+                        alt={item.user.name}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        type="link"
+                        style={{ textDecoration: "none" }}
+                        className="d-block text-white"
+                      >
+                        <h5 className="d-inline mr-2">{item.user.name}</h5>
+                        <h6 className="d-inline" style={{ color: "#ced4da" }}>
+                          {formatLongDate(item.live_at, true)}
+                        </h6>
+                      </Button>
+                    </div>
+                  </Fade>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
