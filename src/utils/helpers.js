c844db39cf451faf0cc7e2474bdb59691f5b0e5c
ikhbal <ikhbaldwiyan@gmail.com>
2024-12-08 16:37:52 +0700

    fix avatar image

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,16 @@
+import axios from "axios";
 export const getLocalStorage = (name) => {
   const data = localStorage.getItem(name);
   return JSON.parse(data) ?? {}
+}
+
+export const getImageBase64 = async (url) => {
+  try {
+    let image = await axios.get(url, { responseType: "arraybuffer" });
+    let data = Buffer.from(image.data).toString("base64");
+    return `data:image/jpeg;base64,${data}`;
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    return ""; // Default image or error placeholder
+  }
 }
